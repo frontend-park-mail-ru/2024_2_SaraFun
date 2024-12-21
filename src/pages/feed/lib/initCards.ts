5f3bd52835675b3018e00341edbe2f3228c527ec
andreykarganov <karganov.an@yandex.ru>
2024-12-21 09:39:03 +0300

    fix feed like

--- src/pages/feed/lib/initCards.ts
+++ src/pages/feed/lib/initCards.ts
@@ -108,9 +108,9 @@ export function initCards(tinderContainer: HTMLElement, parent: any): void {
             firstCard.style.transform = 'translate(' + toX + 'px, ' + toY + 'px) rotate(' + rotate + 'deg)';
 
             let love = deltaX > 0;
-            let userId = firstCard.getAttribute('data-item-id');
+            let username = firstCard.getAttribute('data-item-id');
 
-            let response = await putLikeOrDislike(love, parseInt(userId));
+            let response = await putLikeOrDislike(love, username);
             if ((response as string).trim() === 'у вас нет лайков') {
                 openNotificationModal(parent);
                 firstCard.style.transform = '';
